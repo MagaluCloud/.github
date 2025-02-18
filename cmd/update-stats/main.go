b677105c289ecb491adb026d8e9df8bed39ff1cc
Felipe Publio <felipe@publio.dev>
2025-02-18 12:50:07 -0300

    Refactor update-stats workflow to use Go program for fetching repository stats and update README template

---
+++ cmd/update-stats/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+type RepoStats struct {
+	Stars int `json:"stargazers_count"`
+	Forks int `json:"forks_count"`
+}
+
+type TemplateData struct {
+	Mgccli   RepoStats
+	Provider RepoStats
+	Examples RepoStats
+	Sdk      RepoStats
+}
+
+func fetchRepoStats(repo string) (RepoStats, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/MagaluCloud/%s", repo)
+	resp, err := http.Get(url)
+	if err != nil {
+		return RepoStats{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RepoStats{}, err
+	}
+
+	var stats RepoStats
+	if err := json.Unmarshal(body, &stats); err != nil {
+		return RepoStats{}, err
+	}
+
+	return stats, nil
+}
+
+func main() {
+	basePath := filepath.Join("..", "..")
+
+	data := TemplateData{}
+
+	mgccli, err := fetchRepoStats("mgccli")
+	if err != nil {
+		fmt.Printf("Error fetching mgccli stats: %v\n", err)
+		return
+	}
+	data.Mgccli = mgccli
+
+	provider, err := fetchRepoStats("terraform-provider-mgc")
+	if err != nil {
+		fmt.Printf("Error fetching provider stats: %v\n", err)
+		return
+	}
+	data.Provider = provider
+
+	examples, err := fetchRepoStats("terraform-examples")
+	if err != nil {
+		fmt.Printf("Error fetching examples stats: %v\n", err)
+		return
+	}
+	data.Examples = examples
+
+	sdk, err := fetchRepoStats("mgc-sdk-go")
+	if err != nil {
+		fmt.Printf("Error fetching SDK stats: %v\n", err)
+		return
+	}
+	data.Sdk = sdk
+
+	tmpl, err := template.ParseFiles(filepath.Join(basePath, "cmd/update-stats/README.tmpl"))
+	if err != nil {
+		fmt.Printf("Error parsing template: %v\n", err)
+		return
+	}
+
+	output, err := os.Create(filepath.Join(basePath, "profile/README.md"))
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		return
+	}
+	defer output.Close()
+
+	if err := tmpl.Execute(output, data); err != nil {
+		fmt.Printf("Error executing template: %v\n", err)
+		return
+	}
+
+	fmt.Println("README.md has been successfully updated!")
+}
